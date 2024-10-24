fa725181a4ab6161535aee27f13cc1523ffc3d11
Rodrigo Harispe <rodrigo92hw@gmail.com>
2024-10-23 21:20:45 -0300

    Initial commit

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return (
+        <main className="not-found">
+            <h1>Meal found</h1>
+            <p>Could not reach the page or meal requested.</p>
+        </main>
+    )
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return (
+        <main className="not-found">
+            <h1>Not found</h1>
+            <p>Could not reach the page or resource requested.</p>
+        </main>
+    )
+}
